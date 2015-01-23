dd0a04deb8a1fdea74b9c9cc28e97b5115eadfca
Delermando <delsantos@hotmail.com.br>
2015-01-23 13:41:49 -0200

    adicionado FormTeste

---
+++ demo/protected/models/FormLoginTeste.php
@@ -0,0 +1,20 @@
+<?php
+
+class FormLoginTeste {
+
+    public $model01;
+    private $model02;
+
+    public function __construct() {
+        
+    }
+
+    public function model01() {
+        
+    }
+
+    private function model02() {
+        
+    }
+
+}
